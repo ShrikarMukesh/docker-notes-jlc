6e5d77fa8b49925a9ff707933cefb30473ea3318
Shrikar <ctshrikar@gmail.com>
2021-12-08 23:01:41 +0530

    second commit

---
+++ haiapp/Hai.java
@@ -0,0 +1,6 @@
+public class Hai{
+        public static void main(String as[]){
+                System.out.println("Hello World Shrikar !!!");
+                System.out.println("--You are learning Docker from Srinivas Dande--");
+        }
+}
---
+++ helloapp/Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+        public static void main(String as[]){
+                System.out.println("Hello Guys, welcome to JLC Docker class !!!");
+                System.out.println("--You are learning Docker from Srinivas Dande--");
+        }
+}
